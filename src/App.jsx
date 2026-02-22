848afd7ee3145ee05a70b5a28f06162432348636
LegenD742 <devansh7420@gmail.com>
2026-02-22 19:49:22 +0530

    11

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Appp me hai !! hehehe
+      </h1>
+      <h2>
+        APp me hai ?!
+      </h2>
+    </div>
+  )
+}
+
+export default App
